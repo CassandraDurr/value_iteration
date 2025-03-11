0eb766b8f2e918cb1fa21836c3f131811f0d60b8
Durr <durrc@lancaster.ac.uk>
2025-03-11 12:13:30 +0000

    Fix: value iteration imports

--- value_iteration/__init__.py
+++ value_iteration/__init__.py
@@ -1,3 +1,5 @@
 """Init for public module value_iteration."""
 
-from algorithms import load_mdp_from_csv, validate_mdp  # noqa: F401
+from algorithms import load_mdp_from_csv  # noqa: F401
+from algorithms import validate_mdp  # noqa: F401
+from algorithms import value_iteration  # noqa: F401
