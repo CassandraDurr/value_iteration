0f6994dd4057ee869cde49984fef0c688274e4d6
Durr <durrc@lancaster.ac.uk>
2025-03-15 11:36:01 +0000

    Introduce class-based structure

--- value_iteration/__init__.py
+++ value_iteration/__init__.py
@@ -1,4 +1,4 @@
 """Init for module value_iteration."""
 
-from .algorithms import value_iteration  # noqa: F401
+from .algorithms import AsynchValueIteration, ValueIteration  # noqa: F401
 from .mdp import MDP, load_mdp_from_csv  # noqa: F401
--- value_iteration/algorithms.py
+++ value_iteration/algorithms.py
@@ -6,234 +6,233 @@ from typing import Any
 from .mdp import MDP
 
 
-def extract_policy_from_values(
-    mdp: MDP,
-    values: dict[Any, float],
-    gamma: float = 0.9,
-) -> dict:
-    """Given Markov Decision Process (MDP) variables and value function, return the optimal policy.
+class ValueIteration:
+    """Synchronous value iteration class for Markov Decision Processes (MDPs)."""
 
-    Args:
-        mdp (MDP): Data class containing states, actions, rewards and probabilities.
-        values (dict[Any, float]): Value function of states.
-        gamma (float, optional): Discount factor. Defaults to 0.9.
+    def __init__(
+        self, mdp: MDP, gamma: float = 0.9, theta: float = 1e-6, printing: bool = False
+    ) -> None:
+        """Initialise the value iteration class performing synchronous value iteration.
 
-    Returns:
-        dict: Optimal policy - mapping of states to best action.
-    """
-    # Extract policy
-    policy = {}
-    for s in mdp.states:
-        # Initialise best action and value for state
-        best_action = None
-        best_value = float("-inf")
+        Args:
+            mdp (MDP): Data class containing states, actions, rewards and probabilities.
+            gamma (float, optional): Discount factor. Defaults to 0.9.
+            theta (float, optional): Convergence threshold. Defaults to 1e-6.
+            printing (bool, optional): If True, prints out iteration updates. Defaults to False.
 
-        for a in mdp.actions[s]:
-            q_value = mdp.rewards.get((s, a), 0) + gamma * sum(
-                mdp.probabilities.get((s, a, s_), 0) * values[s_] for s_ in mdp.states
-            )
+        Raises:
+            ValueError: Parameter for gamma is not between 0 and 1.
+            ValueError: Parameter for theta is negative.
+        """
+        # Validate value iteration parameters
+        if gamma > 1 or gamma < 0:
+            raise ValueError("Discount factor gamma must be between 0 and 1.")
 
-            if q_value > best_value:
-                best_value = q_value
-                best_action = a
+        if theta <= 0:
+            raise ValueError("Convergence threshold theta must not be negative.")
 
-        policy[s] = best_action
+        # Assign parameters
+        self.mdp = mdp
+        self.gamma = gamma
+        self.theta = theta
+        self.printing = printing
 
-    return policy
+        # Initialise value function as zero for all states
+        self.values = dict.fromkeys(self.mdp.states, 0.0)
 
+    def get_q_value(self, s: Any, a: Any) -> float:
+        """Get the Q-value of a state-action pair (s,a) given the value function.
 
-def value_iteration(
-    mdp: MDP,
-    gamma: float = 0.9,
-    theta: float = 1e-6,
-    printing: bool = False,
-) -> tuple[dict[Any, float], dict]:
-    """Perform value iteration and strict input validation.
+        Args:
+            s (Any): State of the MDP.
+            a (Any): Action taken at state.
+            values (dict[Any, float]): Value function of states.
 
-    Args:
-        mdp (MDP): Data class containing states, actions, rewards and probabilities.
-        gamma (float, optional): Discount factor. Defaults to 0.9.
-        theta (float, optional): Convergence threshold. Defaults to 1e-6.
-        printing (bool, optional): If True, prints out iteration updates. Defaults to False.
+        Returns:
+            float: Q-value.
+        """
+        return self.mdp.rewards.get((s, a), 0) + self.gamma * sum(
+            self.mdp.probabilities.get((s, a, s_), 0) * self.values[s_]
+            for s_ in self.mdp.states
+        )
 
-    Raises:
-        ValueError: Discount factor gamma must be between 0 and 1.
-        ValueError: Convergence threshold theta must not be negative.
+    def extract_policy(self) -> dict:
+        """Given MDP return the optimal policy.
 
-    Returns:
-        tuple[dict[Any, float], dict]: Optimal values, Optimal policy
-            Optimal values: Mapping of states to the optimal state value
-            Optimal policy: Mapping of states to the best action to take in that state
-    """
-    # Validate value iteration parameters
-    if gamma > 1 or gamma < 0:
-        raise ValueError("Discount factor gamma must be between 0 and 1.")
+        Returns:
+            dict: Optimal policy - mapping of states to best action.
+        """
+        # Extract policy
+        policy = {}
+        for s in self.mdp.states:
+            # Initialise best action and value for state
+            best_action = None
+            best_value = float("-inf")
 
-    if theta <= 0:
-        raise ValueError("Convergence threshold theta must not be negative.")
+            for a in self.mdp.actions[s]:
+                q_value = self.get_q_value(s=s, a=a)
 
-    # Initialise value function as zero for all states
-    values = dict.fromkeys(mdp.states, 0.0)
+                if q_value > best_value:
+                    best_value = q_value
+                    best_action = a
 
-    # Track iterations
-    iteration = 0
-    while True:
-        iteration += 1
+            policy[s] = best_action
 
-        # Copy existing value function
-        new_values = values.copy()
+        return policy
 
-        # Track changes in the value function for convergence
-        delta_values = []
+    def value_iteration(self) -> tuple[dict[Any, float], dict]:
+        """Perform synchronous value iteration.
 
-        for s in mdp.states:
-            # If no valid actions exist for this state, pass
-            if s not in mdp.actions or not mdp.actions[s]:
-                continue
+        Returns:
+            tuple[dict[Any, float], dict]: Optimal values, Optimal policy
+                Optimal values: Mapping of states to the optimal state value
+                Optimal policy: Mapping of states to the best action to take in that state
+        """
+        # Track iterations
+        iteration = 0
+        while True:
+            iteration += 1
 
-            # Initialise list to store Q values for the state
-            q_values = []
+            # Copy existing value function
+            new_values = self.values.copy()
 
-            # Get Q for all actions for this state
-            for a in mdp.actions[s]:
-                q_values.append(
-                    mdp.rewards.get((s, a), 0)
-                    + gamma
-                    * sum(
-                        mdp.probabilities.get((s, a, s_), 0) * values[s_]
-                        for s_ in mdp.states
-                    )
-                )
+            # Track changes in the value function for convergence
+            delta_values = []
 
-            # Update value function for the state
-            new_values[s] = max(q_values)
+            for s in self.mdp.states:
+                # If no valid actions exist for this state, pass
+                if s not in self.mdp.actions or not self.mdp.actions[s]:
+                    continue
 
-            # Calculate absolute change in value function for the state
-            delta_values.append(abs(values[s] - new_values[s]))
+                # Initialise list to store Q values for the state
+                q_values = []
 
-        # Update value function
-        values = new_values
+                # Get Q for all actions for this state
+                for a in self.mdp.actions[s]:
+                    q_values.append(self.get_q_value(s=s, a=a))
 
-        if printing:
-            print(f"Iteration {iteration}, max value change: {max(delta_values)}")
+                # Update value function for the state
+                new_values[s] = max(q_values)
 
-        # Test for convergence
-        if max(delta_values) < theta:
-            break
+                # Calculate absolute change in value function for the state
+                delta_values.append(abs(self.values[s] - new_values[s]))
 
-    # Extract policy
-    policy = extract_policy_from_values(mdp, values, gamma)
+            # Update value function
+            self.values = new_values
 
-    return values, policy
+            if self.printing:
+                print(f"Iteration {iteration}, max value change: {max(delta_values)}")
 
+            # Test for convergence
+            if max(delta_values) < self.theta:
+                break
 
-def extract_policy_from_q_table(
-    mdp: MDP, q_table: dict[tuple[Any, Any], float]
-) -> dict:
-    """Given Markov Decision Process (MDP) variables and Q-values, return the optimal policy.
+        # Extract policy
+        policy = self.extract_policy()
 
-    Args:
-        mdp (MDP): Data class containing states, actions, rewards and probabilities.
-        q_table (dict[tuple[Any, Any], float]): Q-values associated with state-action pairs.
-
-    Returns:
-        dict: Optimal policy - mapping of states to best action.
-    """
-    policy = {}
-    for state in mdp.states:
-        # Get all actions for the current state
-        actions = mdp.actions.get(state, [])
-
-        # Choose the action with the maximum Q-value
-        best_action = None
-        max_q_value = float("-inf")
-
-        for action in actions:
-            q_value = q_table.get((state, action), 0)
-            if q_value > max_q_value:
-                max_q_value = q_value
-                best_action = action
-
-        # Assign the best action for the state
-        policy[state] = best_action
-
-    return policy
+        return self.values, policy
 
 
-def asynchronous_value_iteration(
-    mdp: MDP,
-    gamma: float = 0.9,
-    theta: float = 1e-6,
-    printing: bool = False,
-    n: int = 10,
-) -> tuple[dict[Any, float], dict]:
-    """Perform value iteration and strict input validation.
+class AsynchValueIteration(ValueIteration):
+    """Asynchronous value iteration class for Markov Decision Processes (MDPs)."""
 
-    Args:
-        mdp (MDP): Data class containing states, actions, rewards and probabilities.
-        gamma (float, optional): Discount factor. Defaults to 0.9.
-        theta (float, optional): Convergence threshold. Defaults to 1e-6.
-        printing (bool, optional): If True, prints out iteration updates. Defaults to False.
-        n (int, optional): Iterations to consider in convergence calculation. Defaults to 10.
+    def __init__(  # pylint: disable=too-many-positional-arguments
+        self,
+        mdp: MDP,
+        gamma: float = 0.9,
+        theta: float = 1e-6,
+        printing: bool = False,
+        n: int = 10,
+    ):
+        """Initialise the value iteration class performing asynchronous value iteration.
 
-    Raises:
-        ValueError: Discount factor gamma must be between 0 and 1.
-        ValueError: Convergence threshold theta must not be negative.
+        Args:
+            mdp (MDP): Data class containing states, actions, rewards and probabilities.
+            gamma (float, optional): Discount factor. Defaults to 0.9.
+            theta (float, optional): Convergence threshold. Defaults to 1e-6.
+            printing (bool, optional): If True, prints out iteration updates. Defaults to False.
+            n (int, optional): Convergence parameter for the number of prior iterations to consider.
+                Defaults to 10.
+        """
+        super().__init__(mdp, gamma, theta, printing)
+        self.n = n
+        # Get valid state-action pairs for Q-values
+        self.valid_state_actions = {
+            (state, action)
+            for state, actions in mdp.actions.items()
+            for action in actions
+        }
 
-    Returns:
+        # Initialise Q-values as zero for all state-action pairs
+        self.q_table = dict.fromkeys(self.valid_state_actions, 0.0)
+
+    def get_q_value(self, s: Any, a: Any) -> float:
+        """Get the Q-value of a state-action pair (s,a).
+
+        Args:
+            s (Any): State of the MDP.
+            a (Any): Action taken at state.
+
+        Returns:
+            float: Q-value.
+        """
+        return self.q_table.get((s, a), 0)
+
+    def update_q_table(self, s: Any, a: Any) -> None:
+        """Update the Q-value for state s and action a.
+
+        Args:
+            s (Any): State of the MDP.
+            a (Any): Action taken at state.
+        """
+        self.q_table[(s, a)] = self.mdp.rewards.get((s, a), 0) + self.gamma * sum(
+            self.mdp.probabilities.get((s, a, s_), 0)
+            * max(self.q_table.get((s_, a_), 0) for a_ in self.mdp.actions.get(s_, []))
+            for s_ in self.mdp.states
+        )
+
+    def value_iteration(self) -> tuple[dict[Any, float], dict]:
+        """Perform asynchronous value iteration.
+
+        Returns:
         tuple[dict[Any, float], dict]: Optimal Q table, Optimal policy
             Optimal Q table: Mapping of states to the optimal state value
             Optimal policy: Mapping of states to the best action to take in that state
-    """
-    # Validate value iteration parameters
-    if gamma > 1 or gamma < 0:
-        raise ValueError("Discount factor gamma must be between 0 and 1.")
+        """
+        # Track changes in Q-values for convergence
+        delta_history = []
 
-    if theta <= 0:
-        raise ValueError("Convergence threshold theta must not be negative.")
+        # Track iterations
+        iteration = 0
+        while True:
+            iteration += 1
 
-    # Get valid state-action pairs for Q-values
-    valid_state_actions = {
-        (state, action) for state, actions in mdp.actions.items() for action in actions
-    }
+            # Sample a valid state-action pair
+            s, a = random.choice(list(self.valid_state_actions))
 
-    # Initialise Q-values as zero for all state-action pairs
-    q_table = dict.fromkeys(valid_state_actions, 0.0)
+            # Compute the Q-value update
+            old_q_value = self.q_table[(s, a)]
+            self.update_q_table(s=s, a=a)
 
-    # Track changes in Q-values for convergence
-    delta_history = []
+            # Obtain the change in the Q-value
+            delta = abs(self.q_table[(s, a)] - old_q_value)
+            delta_history.append(delta)
 
-    # Track iterations
-    iteration = 0
-    while True:
-        iteration += 1
+            # Check for convergence based on average delta over last n iterations
+            if len(delta_history) >= self.n:
+                # Calculate the average of last n delta values
+                average_delta = (
+                    sum(delta_history[-self.n :])  # noqa: E203 - conflict with black
+                    / self.n
+                )
 
-        # Sample a valid state-action pair
-        s, a = random.choice(list(valid_state_actions))
+                if self.printing:
+                    print(f"Iteration {iteration}, Average delta: {average_delta}")
 
-        # Compute the Q-value update
-        old_q_value = q_table[(s, a)]
-        q_table[(s, a)] = mdp.rewards.get((s, a), 0) + gamma * sum(
-            mdp.probabilities.get((s, a, s_), 0)
-            * max(q_table.get((s_, a_), 0) for a_ in mdp.actions.get(s_, []))
-            for s_ in mdp.states
-        )
+                if average_delta < self.theta:
+                    break
 
-        # Obtain the change in the Q-value
-        delta_history.append(abs(q_table[(s, a)] - old_q_value))
+        # Derive policy from Q-values
+        policy = self.extract_policy()
 
-        # Check for convergence based on average delta over last n iterations
-        if len(delta_history) >= n:
-            # Calculate the average of last n delta values
-            average_delta = sum(delta_history[-n:]) / n
-
-            if printing:
-                print(f"Iteration {iteration}, Average delta: {average_delta}")
-
-            if average_delta < theta:
-                break
-
-    # Derive policy from Q-values
-    policy = extract_policy_from_q_table(mdp=mdp, q_table=q_table)
-
-    return q_table, policy
+        return self.q_table, policy
