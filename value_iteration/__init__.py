5e89a79b3871c6a6f08ebf1a6905fde18165fbc8
Durr <durrc@lancaster.ac.uk>
2025-03-19 11:30:05 +0000

    Improve layout of pdoc init file

--- value_iteration/__init__.py
+++ value_iteration/__init__.py
@@ -1,4 +1,14 @@
-"""Init for module value_iteration."""
+"""
+# Value Iteration
+
+This package provides implementations of value iteration for Markov Decision Processes (MDPs).
+Two algorithms are provided - a synchronous value iteration algorithm, and an asynchronous version.
+
+## Features:
+- Define MDP environments
+- Allow users to define MDP variables in plain Python or through csv format.
+- Implement the value iteration algorithm synchronously and asynchronously.
+"""
 
 from .algorithms import AsynchValueIteration, ValueIteration  # noqa: F401
 from .mdp import MDP, load_mdp_from_csv  # noqa: F401
